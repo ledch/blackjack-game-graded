64a1c2faed050cb508309789b7c4aae0539566ce
Amelia <yupeiho@gmail.com>
2023-08-10 14:40:27 -0700

    Modifications to blackjack game:

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
-//starting out with false, because how can you be alive when you're not even playing the game?
 let isAlive = true;
 let messageEl = document.querySelector(".message-el");
 let sumEl = document.querySelector(".sum-el");
 let cardsEl = document.querySelector(".cards-el");
+let startBtn = document.querySelector(".start-btn");
+let newCardBtn = document.querySelector(".new-card-btn")
 
 //populate player name and number of chips
 let player = {
@@ -16,6 +17,34 @@ let player = {
 let playerEl = document.querySelector(".player-el");
 playerEl.textContent = player.name + ": $" + player.chips;
 
+//player takes a turn, getting a card
+function takeTurn() {
+    getRandomCard();
+    checkSum();
+    renderGame();
+    checkGameState();
+}
+
+function getRandomCard() {
+    let randomCard = (Math.floor(Math.random()*13) + 1);
+   if (randomCard===1) {
+    return 11;
+   }
+   else if (randomCard>=11) {
+    return 10;
+   }
+   else {
+    return randomCard;
+   }
+}
+
+function checkSum() {
+    let cardValue = getRandomCard();
+    cards.push(cardValue);
+    sum+=cardValue;
+}
+
+//prints out the messages
 function renderGame() {
     cardsEl.textContent = "Cards:";
     for (let i=0; i<cards.length; i++) {
@@ -31,40 +60,25 @@ function renderGame() {
         message = "You're out of the game!";
         isAlive = false;
     }
-    // 2. Display the message in the messageEl using messageEl.textContent
     messageEl.textContent = message;
 }
 
-function startGame() {
-    if (isAlive && hasBlackJack!==true) {
-        let card = getRandomCard();
-        cards.push(card);
-        sum+=card;
-        renderGame();
+function checkGameState() {
+    //enable start button, disable new card button if you get blackjack or lose
+    if (!isAlive || hasBlackJack===true) {
+        startBtn.disabled = false;
+        newCardBtn.disabled = true;
+        resetGame();
     }
+    //if you are still playing only new card is enabled
     else {
-        isAlive=true;
-        sum = 0;
-        cards=[];
-        renderGame();
+        startBtn.disabled=true;
+        newCardBtn.disabled = false;
     }
 }
 
-function getRandomCard() {
-    let randomNumber = (Math.floor(Math.random()*13) + 1);
-   if (randomNumber===1) {
-    return 11;
-   }
-   else if (randomNumber>=11) {
-    return 10;
-   }
-   else {
-    return randomNumber;
-   }
-}
-
-function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
-        startGame();
-    }
+function resetGame() {
+    isAlive=true;
+    sum = 0;
+    cards=[];
 }
